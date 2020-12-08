097eaae6e1e353d098b081347e3013fc3e08f5f2
nochevassu <nochevassu@iutclinfa1805l.iut.local>
2020-12-08 19:34:18 +0100

    Changement arborescence et ajout de la vue page accueil ainsi que controleur

--- src/main/java/launch/Launch.java
+++ src/main/java/launch/Launch.java
@@ -4,14 +4,20 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
 import javafx.stage.Stage;
 
 public class Launch extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-        Parent root = FXMLLoader.load(getClass()).getResource(/fxml/Vue.fxml));
-        Scene scene = new Scene(root);
+        Parent root = FXMLLoader.load(getClass().getResource("/fxml/VuePageAccueil.fxml"));
+        Scene scene = new Scene(root, 500, 500);
+        scene.getStylesheets().add(getClass().getResource("/css/application.css").toExternalForm());
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+    public static void main(String[] args){
+        launch(args);
     }
 }
---
+++ src/main/java/view/PageAccueil.java
@@ -0,0 +1,11 @@
+package view;
+
+import javafx.fxml.FXML;
+
+public class PageAccueil {
+
+    @FXML
+    private void clicBouton(){
+
+    }
+}
